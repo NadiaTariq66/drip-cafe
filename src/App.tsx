66fbfb3b5e662c98ddff3eca6575c8ceb936efab
NadiaTariq66 <tariqnadia830@gmail.com>
2026-07-25 03:38:50 +0500

    Wire app routes and React entry point.

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { About } from './pages/About'
+import { Automation } from './pages/Automation'
+import { Gallery } from './pages/Gallery'
+import { Home } from './pages/Home'
+import { Menu } from './pages/Menu'
+import { Reserve } from './pages/Reserve'
+import { Ritual } from './pages/Ritual'
+import { Visit } from './pages/Visit'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="about" element={<About />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="visit" element={<Visit />} />
+          <Route path="reserve" element={<Reserve />} />
+          <Route path="ritual" element={<Ritual />} />
+          <Route path="automation" element={<Automation />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
